6b570153e913be36c009460422bb5e93fe5307bd
James Laster <jlaster@five-startech.com>
2020-08-24 21:39:14 -0400

    fix

--- spin.js
+++ spin.js
@@ -1,14 +1,18 @@
 // 200 steps per rotation
 
-require('./lib/Stepper')
+var Stepper = require('./lib/Stepper')
 
 const motor = new stepper(21, 22, 19)
 
 motor.direction('cw')
 motor.enable()
 
-var stepInterval = setInterval(function(){motor.step()}, 50)
+var stepInterval = setInterval(function(){
+    motor.step()
+}, 50)
 
-setTimeout(motor.destroy, 5000);
+setTimeout(function(){
+    motor.destroy()
+}, 5000);
 
 clearInterval(stepInterval);
